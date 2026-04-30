90bec541ac4782c4032e7ed7dd604cdeb2ba53e9
levivi77708 <levivi77708@gmail.com>
2026-04-30 12:06:20 -0700

    Update tester comments

--- src/Tester.java
+++ src/Tester.java
@@ -1,32 +1,16 @@
 /**
- * Tester class for creating and printing sample Hike objects.
+ * Tester class for launching the hiking tracker application.
+ * It creates a User instance and starts the interactive menu.
  */
 public class Tester
 {
     /**
-     * Main entry point for the tester.
+     * Main entry point for the application.
      * @param args command-line arguments (not used)
      */
     public static void main(String[] args)
     {
-        Hike hike1 = new Hike("Torrey Pines Trail", 3.5, "04/10/2026");
-        Hike hike2 = new Hike("Cowles Mountain", 5.0, "04/11/2026");
-        Hike hike3 = new Hike("Sunset Cliffs Walk", 2.0, "04/12/2026");
-
-        Hiker hiker = new Hiker("Vivi", 17);
-
-        hiker.addHike(hike1);
-        hiker.addHike(hike2);
-        hiker.addHike(hike3);
-
-
-
-        System.out.println("Hikes:");
-        hiker.printHikes();
-
-        System.out.println();
-        System.out.println("Total Miles: " + hiker.getTotalMiles());
-
-
+        User user = new User();
+        user.start();
     }
 }
